2416a58859427bda68d76f0540f63c945e51827b
Katerina <dmitrieva4446@mail.ru>
2023-07-25 22:40:44 +0200

    ДЗ 7

---
+++ 52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6;
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void AverageColumnMatrix(int[,] matrix)
+{
+    double sum = 0;
+    double Average = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+            Average = Math.Round(sum / matrix.GetLength(0), 1);
+        }
+        Console.Write($"{Average} \t");
+        sum = 0;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы через пробел: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine($"_________________________________________");
+AverageColumnMatrix(matrix);
